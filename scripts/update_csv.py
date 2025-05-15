b32b62d6192ba57908de573ed73b430fa132c399
shubham_G <shubhamgawali.sag@gmail.com>
2025-05-15 17:14:48 +0530

    Update update_csv.py

--- scripts/update_csv.py
+++ scripts/update_csv.py
@@ -24,10 +24,6 @@ for folder in folders:
 
     # For 'masterdata', fetch top 6 full-report files
     if folder == "masterdata":
-        cmd_list_files = f"mc ls --json {MINIO_ALIAS}/{folder_path}/ | grep 'full-report' | sort -r | head -6"
-        file_output = subprocess.getoutput(cmd_list_files)
-        file_lines = [line.strip() for line in file_output.strip().split("\n") if line.strip()]
-    if folder == "masterdata":
         cmd_list_files = f"mc ls --json ${MINIO_ALIAS}/${folder_path}/ | grep 'full-report' | sort -r | tail -n +7 | head -6"
         file_output = subprocess.getoutput(cmd_list_files)
         file_lines = [line.strip() for line in file_output.strip().split("\n") if line.strip()]
