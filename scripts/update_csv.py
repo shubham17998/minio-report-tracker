c5760745d3ed719b7b31d83e2688d7f5a8826fcf
shubham_G <shubhamgawali.sag@gmail.com>
2025-05-15 13:45:55 +0530

    Update update_csv.py

--- scripts/update_csv.py
+++ scripts/update_csv.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 # MinIO alias and bucket
 MINIO_ALIAS = "myminio"
-MINIO_BUCKET = "apitestrig"
+MINIO_BUCKET = "automation"
 
 # CSV file path
 csv_path = "../spreadsheet/reports.csv"
